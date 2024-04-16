09018555dd88a83c225f08d9ddd2e4426f1d5a57
Ankit Mishra <anki.mishra@gmail.com>
2024-04-16 08:23:28 +0530

    Update connection.py

--- db/connection.py
+++ db/connection.py
@@ -2,11 +2,19 @@ import pymongo
 from pymongo import MongoClient
 from pymongo import collection
 
-cluster = MongoClient(
-    'mongodb+srv://dilsedigital007:wh1teMayur@cluster0.opahplu.mongodb.net/Mongodb?retryWrites=true&w=majority')
 
-db = cluster["sldb"]
-collection = db["employee"]
-post = {"name": "amar", "email": "amar@mail.com"}
+def getDBConnection(username, password, database):
+    cluster = MongoClient(
+        'mongodb+srv://' + username + ':' + password + '@cluster0.opahplu.mongodb.net/' + database + '?retryWrites=true&w=majority')
 
-collection.insert_one(post)
+    #    cluster = MongoClient(
+    #        'mongodb+srv://dilsedigital007:wh1teMayur@cluster0.opahplu.mongodb.net/Mongodb?retryWrites=true&w=majority')
+
+    db = cluster["sldb"]
+    collection = db["employee"]
+    post = {"name": "amar", "email": "amar@mail.com"}
+
+    collection.insert_one(post)
+
+
+getDBConnection('dilsedigital007', 'wh1teMayur', 'Mongodb')
