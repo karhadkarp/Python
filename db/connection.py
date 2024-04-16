af05a70c1e09a25c23f54b09eaac9144f75f13f7
Ankit Mishra <anki.mishra@gmail.com>
2024-04-16 08:13:23 +0530

    Update connection.py

--- db/connection.py
+++ db/connection.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 from pymongo import collection
 
 cluster = MongoClient(
-    'mongodb+srv://dilsedigital007:wh1teMayur@cluster0.opahplu.mongodb.net/MongoDB?retryWrites=true&w=majority')
+    'mongodb+srv://dilsedigital007:wh1teMayur@cluster0.opahplu.mongodb.net/Mongodb?retryWrites=true&w=majority')
 
 db = cluster["sldb"]
 collection = db["employee"]
