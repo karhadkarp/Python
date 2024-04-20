9f7cf6281b5720e902cfa150acb2330d4cfc1276
nishantdoshi0909 <88325252+nishantdoshi0909@users.noreply.github.com>
2024-04-20 22:55:25 +0530

    Update Get_customer_info.py
    added code to return Json

--- Get_customer_info.py
+++ Get_customer_info.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[6]:
+# In[9]:
 
 
 import pandas as pd
 from pymongo import MongoClient
+import json
 
 def get_customer_info(customer_id):
     client = MongoClient('mongodb+srv://dilsedigital007:wh1teMayur@cluster0.opahplu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
@@ -21,14 +22,18 @@ def get_customer_info(customer_id):
     cust_name = customer_info['Custname'].values[0]
     sug_products = customer_info[['SugProdID1', 'SugProdName1', 'SugProdID2', 'SugProdName2', 'SugProdID3', 'SugProdName3']]
     sug_products_list = sug_products.values.tolist()[0]
-    sug_products_2d = [[sug_products_list[i], sug_products_list[i+1]] for i in range(0, len(sug_products_list), 2)]
-    return cust_name, sug_products_2d
+    sug_products_2d = [{'SugProdID': sug_products_list[i], 'SugProdName': sug_products_list[i+1]} for i in range(0, len(sug_products_list), 2)]
+    result_json = {
+        'cust_name': cust_name,
+        'sug_products': sug_products_2d
+    }
+    return json.dumps(result_json)
 
 # Example usage:
-customer_id_input = 15737888  # Example customer ID
-cust_name, sug_products = get_customer_info(customer_id_input)
-print("Customer Name:", cust_name)
-print("Suggested Products:",sug_products)
+#customer_id_input = 15737888  # Example customer ID
+#result_json = get_customer_info(customer_id_input)
+#print("Customer Info (JSON format):")
+#print(result_json)
 
 
 # In[ ]:
